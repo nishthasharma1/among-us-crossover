f0d6a7d45e9c9029ce21e4365a0bc2add07f09b6
Glitch (wiry-plaid-valley) <none>
2021-07-08 16:13:44 +0000

    🗃🎠 Checkpoint ./olympic rings.js:31745905/549

--- olympic rings.js
+++ olympic rings.js
@@ -1,18 +1,21 @@
-/* List p5 functions here so glitch
+// List p5 functions below so glitch will recognize them
+/* global createCanvas, background 
 */
 
 // Content behind double slashes is a comment. Use it for plain English notes,
 // or for code that you want to temporarily disable.
 
+// Name: elise, partner: fill in
+
 function setup(){
   // Code here runs only once
-  createCanvas(800, 600)
+  createCanvas(800, 600);
 }
 
 function draw(){
   // Code here runs continuously
-  background(220)
-
-  // ellipse(50, 50, 50, 50)
+  background(220);
+  stroke(100,0,0)
+  ellipse(50, 50, 50, 50)
 
 }
