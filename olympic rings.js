2f53df6ec86697597673b6ad8122b613497fb8ea
Glitch (wiry-plaid-valley) <none>
2021-07-12 14:22:36 +0000

    🍞💑 Checkpoint ./olympic rings.js:14211563/651:33212267/395

--- olympic rings.js
+++ olympic rings.js
@@ -6,15 +6,46 @@ stroke, strokeWeight
 // Content behind double slashes is a comment. Use it for plain English notes,
 // or for code that you want to temporarily disable.
 
-// Name: Aldo, partner: fill in
+// Name: Aldo Ruiz, partner: Nishta Sharma
 
 function setup(){
   // Code here runs only once
   createCanvas(800, 600);
 }
 
-function draw(){
-  / Code here runs continuously
+
+function draw (){
+  // fill(R, G, B)
+  //Blue Ellipse
+noFill;
+  stroke(0,0,200);
+  strokeWeight(10);
+  // ellipse(x-coordinate, y-coordinate (upside down), x-width, y-width)
+  ellipse(75, 100, 100, 100);
+// Orange Ellipse 
+    stroke(255,140,0)
+    strokeWeight(10);
+    ellipse(125,160,100,100);
+  
+  
+  
+  
+//Black Ellipose
+  stroke (0,0,0);
+  strokeWeight(10);
+  ellipse(190,100,100,100);
+  
+//Green Ellipse
+  stroke(0,128,0);
+  strokeWeight(10);
+  ellipse(250 , 160, 100, 100);
+//Red Ellipse
+  stroke(178,34,34);
+  strokeWeight(10);
+  ellipse(305,100,100,100);
+  
+  
+  /* Code here runs continuously
   background(220);
 
   // fill(R, G, B)
@@ -35,5 +66,6 @@ function draw(){
   // strokeWeight seems to make the line thicker?
   noFill();
   rect(300, 200, 55, 55);
+  */
 
 }
