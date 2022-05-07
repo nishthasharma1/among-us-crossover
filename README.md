fec253cd47af86e1f072bbdbb9bf166a186c5e06
Glitch (emojiproj) <none>
2022-05-07 05:13:51 +0000

    intro to ml and object classification

---
+++ README.md
@@ -0,0 +1,34 @@
+# Image Classification with Teachable Machine
+
+# Useful Reference Links
+
+- [ML5.js main website](https://learn.ml5js.org/#/)
+- [A Beginner's Guide to Machine Learning with ml5.js (23min)](https://youtu.be/jmznx0Q1fP0)
+
+# Getting Started
+
+1. (Optional) Watch the beginner's guide video (see references above) for an overview. 
+
+1. Some other important things to know about ML...
+
+  Machine learning libraries:
+  - Tensorflow is a well-known open source ML library from Google, for python language.
+  - tf.js is Tensorflow for javascript developers. But a better beginner starting point is...
+  - ML5.js, a javascript library that aims to make ML approachable, from a learning perspective as well as a cost perspective.
+  - Teachable Machine, from Google, building off all of the above and making a user-friendly UI for training models.
+  
+  Machine learning as a field:
+  - ML can be very powerful, handling tasks with more ambiguity such as interpreting handwriting, recognizing faces, etc.
+  - ML is generally quite costly since it has to do an incredible number of calculations, sometimes requiring expensive GPUs.
+  - ML is being increasingly used in many aspects of life, from captcha to facial recognition, and there are many ethical considerations to this.
+  
+1. Get started watching the tutorial video [Teachable Machine 1: Image Classification](https://www.youtube.com/watch?v=kwcillcWOg0). Remix current project for starter code, it's basically the "Code Template".
+  1. Find 3-4 objects around your house that are easy to hold and that you will map to emojis.
+  1. Go to [Teachable Machine website](https://teachablemachine.withgoogle.com/) and choose image project, standard image model.
+  1. Make a class for every object you have and use webcam option to upload about 50-100 images of your object in different positions. Move it around, rotate it, get different angels!
+  1. Train the model (DO NOT LEAVE TAB) and test to see if it's working well. If not, recollect data and re-train.
+  1. Export Model > Tensorflow.js > Upload (shareable link). Hit "Upload My Model" and get the shareable link!
+  1. Come back to glitch, remix this project.
+  1. Follow coding steps in video! There's also a final code link in the description if you are stuck.
+  1. Feel free to play around and build off this or try some other ML5.js tutorials from The Coding Train!
+  
--- script.js
+++ script.js
@@ -1,221 +1,70 @@
-// CSSI Day 12: Snake
-// Group members: ___, ____
+// CSSI Day 13/14: CYOA, ML emojis
+// Made following the tutorial here: https://www.youtube.com/watch?v=kwcillcWOg0
 
 /*
-global createCanvas, windowWidth, windowHeight, color, colorMode, HSB, background, random, width, height, fill, noStroke, ellipse
-frameRate, text, stroke, noFill, rect, noStroke, round, keyCode, key, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW,
-collideRectRect
+global createCanvas, background, image, createCapture, VIDEO, textSize, textAlign, fill, text, CENTER, 
+width, height, 
 */
 
-let backgroundColor, playerSnake, currentApple, score, gameIsOver, currentBonus;
+// Video
+let video;
+
+let label = "waiting...";
+
+let modelURL = "https://teachablemachine.withgoogle.com/models/xDB8pAglE/";
+let classifier;
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + "model.json");
+}
 
 function setup() {
-  // Canvas & color settings
-  createCanvas(400, 400);
-  colorMode(HSB, 360, 100, 100);
-  backgroundColor = 90;
-  frameRate(12);
-  noStroke();
-  
-  // Set up initial values and initialize objects
-  setupGame();
+  createCanvas(640, 520);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+// STEP 2 classify!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
 }
 
 function draw() {
-  background(backgroundColor);
+  background(0);
 
-  if (!gameIsOver) {
-    // The snake performs the following four methods:
-    playerSnake.move();
-    playerSnake.draw();
-    playerSnake.checkCollisions();
-    playerSnake.checkApples();
-    // The apple needs fewer methods to show up on screen.
-    currentApple.draw();
-    //
-    //playerSnake.checkBonus();
-    //currentBonus.draw();
-  } else {
-    fill(0);
-    text("GAME OVER, press 'r' to replay", 50, 50);
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 30);
+
+  let emoji = "⌨️";
+  if (label == "puppy") {
+    emoji = "🐶";
+  } else if (label == "orange") {
+    emoji = "🍊";
+  } else if (label == "potato") {
+    emoji = "🥔";
   }
-  
-  fill(0);
-  text(`Score: ${score}`, 20, 20);
+
+  textSize(125);
+  text(emoji, width - 150, height - 30);
 }
 
-function setupGame() {
-  // Initial values
-  score = 0;
-  gameIsOver = false;
-  // Initialize objects
-  playerSnake = new Snake();
-  currentApple = new Apple();
-  //currentBonus = new Bonus();
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+  classifyVideo();
 }
-
-function keyPressed() {
-  console.log("key pressed: ", keyCode);
-  if (keyCode === UP_ARROW && playerSnake.direction != 'S') {
-    playerSnake.direction = "N";
-  } else if (keyCode === DOWN_ARROW && playerSnake.direction != 'N') {
-    playerSnake.direction = "S";
-  } else if (keyCode === RIGHT_ARROW && playerSnake.direction != 'W') {
-    playerSnake.direction = "E";
-  } else if (keyCode === LEFT_ARROW && playerSnake.direction != 'E') {
-    playerSnake.direction = "W";
-  } else if (key === 'r') {
-    setupGame();
-  } else {
-    console.log("wrong key");
-  }
-}
-
-class Apple {
-  //implemented apple class
-  constructor() {
-    this.size = 10;
-    this.x = random(width-this.size); // width - 10 is on the canvas but not off it
-    this.y = random(height-this.size);
-    this.color = color(0,100,100);
-  }
-
-  draw() {
-    fill(this.color);
-    rect(this.x, this.y, this.size, this.size);
-  }
-}
-
-/*class Bonus {
-  //implemented Bonus score class
-  constructor() {
-    this.size = 12;
-    this.x = random(width-this.size); // width - 10 is on the canvas but not off it
-    this.y = random(height-this.size);
-    this.color = random(360);
-  }
-
-  draw() {
-    fill(this.color);
-    rect(this.x, this.y, this.size, this.size);
-  }
-}
-*/
-class TailSegment {
-  // Implement TailSegment class
-  constructor(x, y) {
-    this.size = 10;
-    this.x = x;
-    this.y = y;
-    this.color = color('green'); 
-  }
-
-  draw() {
-    fill(this.color);
-    rect(this.x, this.y, this.size, this.size);
-  }
-}
-
-class Snake {
-  constructor() {
-    this.headX = width/2;
-    this.headY = height - 10;
-    this.size = 10;
-    this.direction = 'N';
-    this.stepSize = this.size + 2;
-    // Initialized tail array
-    this.tail = [new TailSegment(this.headX, this.headY)]; //ask more about this
-  }
-
-  move() {
-    if (this.direction === "N") {
-      this.headY -= this.stepSize;
-    } else if (this.direction === "S") {
-      this.headY += this.stepSize;
-    } else if (this.direction === "E") {
-      this.headX += this.stepSize;
-    } else if (this.direction === "W") {
-      this.headX -= this.stepSize;
-    } else {
-      console.log("Error: invalid direction");
-    }
-    // Add new TailSegment to the head, remove TailSegment from end
-    this.tail.pop();
-    this.tail.unshift(new TailSegment(this.headX, this.headY));
-  }
-
-  draw() {
-    // Draw the head first
-    // This place is the only area I want to use stroke (to draw a bigger head)
-    //option1: use snake properties
-    /*
-    stroke(240, 100, 100);
-    fill(240, 100, 100);
-    rect(this.headX, this.headY, this.size, this.size);
-    noStroke(); // remember to noStroke after
-    */
-    //option2: use tail segment properties
-    let head = this.tail[0]; //head is a tailsegment object
-    stroke(head.color);
-    fill(head.color);
-    rect(head.x, head.y, head.size, head.size);
-    noStroke();
-    
-    // Drew other tail segments
-    //this.tail.length
-    for (let i = 1; i < this.tail.length; i++) {
-      let seg = this.tail[i]; //seg is tailsegment objectc
-      fill(seg.color);
-      rect(seg.x, seg.y, seg.size, seg.size);
-    }
-  }
-
-  checkApples() {
-    // If the head of the snake collides with the apple,
-    // make a new apple, increment the score
-    if (collideRectRect(this.headX, this.headY, this.size, this.size, 
-        currentApple.x, currentApple.y, currentApple.size, currentApple.size)){
-      //make a new apple, increment the score
-      currentApple = new Apple;  
-      score += 1;
-    
-      // Add a new segment by duplicating whatever you find at the end of the tail.
-      let end = this.tail[-1]; // end is the last tailsegment object in tail
-      this.tail.push(end);
-    }
-  }
-  /*
-  checkBonus() {
-    if (collideRectRect(this.headX, this.headY, this.size, this.size, 
-        currentBonus.x, currentBonus.y, currentBonus.size, currentBonus.size)){
-      //make a new apple, increment the score
-      currentBonus = new Bonus;  
-      score += 2;
-    
-      // Add a new segment by duplicating whatever you find at the end of the tail.
-      let end = this.tail[-1]; // end is the last tailsegment object in tail
-      this.tail.push(end);
-    }
-  }
-*/
-  checkCollisions() {
-    // Canvas boundary check
-    if ((this.headX < 0 || this.headX >= width) || 
-        (this.headY < 0 || this.headY >= height)) {
-      gameIsOver = true;
-    }
-    
-    //Trigger game over when snake folds into itself, head colliding with body
-    for (let i = 1; i < this.tail.length; i++){
-      let seg = this.tail[i];
-      if (collideRectRect(this.headX, this.headY, this.size, this.size,
-                         seg.X, seg.Y, seg.size)){
-        gameIsOver = true;
-      
-      }
-    }
-  }
-}
-
-
-
